94c1b9ef42073f055a07a30b53aa4a3da4f9068f
Darkglade <30379754+Darkglade@users.noreply.github.com>
2020-01-30 00:36:08 -0500

    Final touchups

--- src/main/java/Koishi/actions/SilentAndRestlessAction.java
+++ src/main/java/Koishi/actions/SilentAndRestlessAction.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.GetAllInBattleInstances;
+import com.megacrit.cardcrawl.powers.MinionPower;
 import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 
 import java.util.Iterator;
@@ -29,7 +30,7 @@ public class SilentAndRestlessAction extends AbstractGameAction {
         if (this.duration == 0.1F && this.target != null) {
             AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.SLASH_HORIZONTAL));
             this.target.damage(this.info);
-            if ((this.target.isDying || this.target.currentHealth <= 0) && !this.target.halfDead) {
+            if ((this.target.isDying || this.target.currentHealth <= 0) && !this.target.halfDead && !this.target.hasPower(MinionPower.POWER_ID)) {
                 Iterator var1 = AbstractDungeon.player.masterDeck.group.iterator();
 
                 AbstractCard c;
--- src/main/java/Koishi/cards/Skills/Uncommon/ReleaseOfTheId.java
+++ src/main/java/Koishi/cards/Skills/Uncommon/ReleaseOfTheId.java
@@ -3,11 +3,17 @@ package Koishi.cards.Skills.Uncommon;
 import Koishi.KoishiMod;
 import Koishi.cards.AbstractDefaultCard;
 import Koishi.cards.AbstractIdCard;
+import Koishi.cards.Attacks.Common.SubconsciousSweep;
+import Koishi.cards.Attacks.Uncommon.HeartAttack;
 import Koishi.characters.KoishiCharacter;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static Koishi.KoishiMod.makeCardPath;
 import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
@@ -35,14 +41,37 @@ public class ReleaseOfTheId extends AbstractDefaultCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         KoishiMod.runAnimation("spellCall");
+        ArrayList<AbstractIdCard> cards = new ArrayList<>();
         for (int i = 0; i < magicNumber; i++) {
             AbstractIdCard card = AbstractIdCard.returnTrulyRandomIdCard();
             if (upgraded) {
                 card.upgrade();
             }
+            cards.add(card);
+        }
+        if (!hasDamageIdCard(cards)) {
+            //Always give the player a damage Id card to avoid softlock scenarios
+            ArrayList<AbstractIdCard> damageIdCards = new ArrayList<>();
+            damageIdCards.add(new SubconsciousSweep());
+            damageIdCards.add(new MassHysteria());
+            damageIdCards.add(new HeartAttack());
+            Collections.shuffle(damageIdCards, AbstractDungeon.cardRandomRng.random);
+            cards.set(0, damageIdCards.get(0));
+        }
+        for (AbstractCard card : cards) {
             AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(card, 1, true, true));
         }
+    }
 
+    private boolean hasDamageIdCard(ArrayList<AbstractIdCard> cards) {
+        for (AbstractIdCard card : cards) {
+            if (card instanceof SubconsciousSweep ||
+                card instanceof MassHysteria ||
+                card instanceof HeartAttack) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
